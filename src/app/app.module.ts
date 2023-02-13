431b8c011b209daeab663871aef3ed5bd14a8636
AlessandroZaza <alessandro.zaza@edu.itspiemonte.it>
2023-02-13 14:24:01 +0100

    add pages 'contact', 'about' + routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
