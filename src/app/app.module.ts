1be791c18a42d911e90835fb20d28a0abfe88454
AlessandroZaza <alessandro.zaza@edu.itspiemonte.it>
2023-02-13 13:57:24 +0100

    add navbar and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ButtonComponent } from './button/button.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent,
-    NavbarComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
