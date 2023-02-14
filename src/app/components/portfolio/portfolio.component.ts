917342a73219c5646336723dd4ad628b01920385
PaoloGippa <paolo.gippa@edu.itspiemonte.it>
2023-02-14 12:15:36 +0100

    bug fixed

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -3,8 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { PortfolioPaoloComponent } from '../portfolio-paolo/portfolio-paolo.component';
 
 const routes: Routes = [
-  { path: '../portfolio-paolo/portfolio-paolo.component.html', component: PortfolioPaoloComponent },
+  {
+    path: '../portfolio-paolo/portfolio-paolo.component.html',
+    component: PortfolioPaoloComponent,
+  },
 ];
+
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
