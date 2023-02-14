54d9ef316baabb21785c9cca71041382eec208da
AlessandroZaza <alessandro.zaza@edu.itspiemonte.it>
2023-02-14 14:51:30 +0100

    adjust contact form and other bugs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { PortfolioPaoloComponent } from './components/portfolio-paolo/portfolio-
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'portfolio', component: PortfolioComponent },
