83e465394130f9662036e004e0dbc5ce1b20fdc3
PaoloGippa <paolo.gippa@edu.itspiemonte.it>
2023-02-15 17:46:54 +0100

    bug fixes

--- src/app/components/preventivo/preventivo.component.ts
+++ src/app/components/preventivo/preventivo.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 
-var arrayDomain = ["www.google.com", "www.facebook.com", "www.microsoft.com", "www.sony.com", "www.nttdata.com"];
+let arrayDomain = [
+  'www.google.com',
+  'www.facebook.com',
+  'www.microsoft.com',
+  'www.sony.com',
+  'www.nttdata.com',
+];
 @Component({
   selector: 'app-preventivo',
   templateUrl: './preventivo.component.html',
-  styleUrls: ['./preventivo.component.css']
+  styleUrls: ['./preventivo.component.css'],
 })
-export class PreventivoComponent {
-
-}
+export class PreventivoComponent {}
