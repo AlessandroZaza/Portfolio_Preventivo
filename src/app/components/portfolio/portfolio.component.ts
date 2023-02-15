c3b35c13ca0055844bfcc89d0c79221d2dadc8d4
PaoloGippa <paolo.gippa@edu.itspiemonte.it>
2023-02-15 14:04:33 +0100

    add many issues at my page, portfolio-paolo.component.html

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,11 +1,16 @@
 import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PortfolioPaoloComponent } from '../portfolio-paolo/portfolio-paolo.component';
+import { PortfolioAlessandroComponent } from '../portfolio-alessandro/portfolio-alessandro.component';
 
 const routes: Routes = [
   {
     path: '../portfolio-paolo/portfolio-paolo.component.html',
     component: PortfolioPaoloComponent,
+  },
+  {
+    path: '../portfolio-alessandro/portfolio-alessandro.component.html',
+    component: PortfolioAlessandroComponent,
   },
 ];
 
