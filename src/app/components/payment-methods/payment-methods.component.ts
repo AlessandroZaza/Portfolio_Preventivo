4ea7be1e14f43273e1144ef7f4b85843dd817af1
AlessandroZaza <alessandro.zaza@edu.itspiemonte.it>
2023-03-17 11:41:14 +0100

    add ts to payment-methods page

--- src/app/components/payment-methods/payment-methods.component.ts
+++ src/app/components/payment-methods/payment-methods.component.ts
@@ -17,6 +17,59 @@ interface Card {
   templateUrl: './payment-methods.component.html',
   styleUrls: ['./payment-methods.component.css'],
 })
-export class PaymentMethodsComponent {
+export class PaymentMethodsComponent implements OnInit {
 
+  loading = false;
+  currentCardIndex : number = 0;
+  cardsDisplay: CreditCardResponse = {
+    data: [
+      {
+        type: '',
+        number: 0,
+        expiration: '',
+        owner: ''
+      }
+    ]
+  }
+
+  private readonly apiAddress = 'https://fakerapi.it/api/v1/credit_cards?_quantity=';
+  private readonly quantity = 5; 
+
+  constructor(public http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loadCards();
+  }
+
+  test( index : number ){ 
+    this.currentCardIndex = index;
+  }
+    
+  loadCards(): void {
+    this.loading = true;
+
+    this.http
+      .get<CreditCardResponse>(
+        `${this.apiAddress}${this.quantity}`
+      )
+      .subscribe((res) => {
+        this.cardsDisplay = res;
+        this.cardsDisplay.data.sort((a, b) => a.number - b.number); // ordina in base al numero della carta
+        //  console.log('-------------------------------------');
+        //  this.cardsDisplay.data.forEach((card) => {
+        //    console.log(
+        //   '| Tipo: ' +
+        //       card.type +
+        //        '\n| Numero: ' +
+        //        card.number +
+        //        '\n| Scadenza: ' +
+        //        card.expiration +
+        //        '\n| Proprietario: ' +
+        //        card.owner 
+        //    );
+        //    console.log('-------------------------------------');
+        //  });
+        this.loading = false;
+      });
+  }
 }
