015fbc462d526977076ab01fa267d58a4b618cc6
AlessandroZaza <alessandro.zaza@edu.itspiemonte.it>
2023-04-20 16:26:06 +0200

    add country filter and adjust table for xl display

--- src/app/components/companies/companies.component.ts
+++ src/app/components/companies/companies.component.ts
@@ -59,7 +59,6 @@ interface addressesObj {
   styleUrls: ['./companies.component.css'],
 })
 export class CompaniesComponent implements OnInit {
-  searchText: any;
   loading = false;
   tableDisplay: ApiResponse = {
     data: [
@@ -110,13 +109,17 @@ export class CompaniesComponent implements OnInit {
       },
     ],
   };
-
-  filteredCompanies: ApiResponse = { data: [] };
-  noFilterTable = true;
-  filterBox = false;
-  filter = '';
-  viewOptions = false;
-  visibleColumns = [true, true, true, true, true, true, true, false];
+  countries: string[] = []; // array degli stati per il filtro a tendina
+  filterText: any; // testo inserito dall'utente per filtrare i nomi
+  filter: string = ''; 
+  selectedCountry: any; // stato selezionato
+  filteredCompanies: ApiResponse = { data: [] }; // tabella filtrata, ora vuota, andrà a popolarsi successivamente
+  noFilterTable = true; // booleana per far apparire o meno la tabella con i filtri o senza
+  filterBoxText = false; // booleana per far apparire il div con il filtro (nome) inserito dall'utente
+  filterBoxCountry = false; // booleana per far apparire il div con il filtro (stato) inserito dall'utente
+  filterBoxWithResults = false; // booleana per far apparire il div con tutti i filtri e il numero di risultati
+  viewOptions = false; // booleana per far apparire il div con le View Options della tabella
+  visibleColumns = [true, true, true, true, true, true, true, false]; // array di booleani per settare quali colonne devono essere visibili o no
 
   private readonly apiAddress =
     'https://fakerapi.it/api/v1/companies?_quantity=';
@@ -135,26 +138,59 @@ export class CompaniesComponent implements OnInit {
       .get<ApiResponse>(`${this.apiAddress}${this.quantity}`)
       .subscribe((res) => {
         this.tableDisplay = res;
+
+        // funzione per popolare l'array countries e ordinarlo 
+        this.tableDisplay.data.forEach(company => { this.countries.push(company.country)}); 
+        this.countries.sort((a,b)=> {
+          if (a > b) { return 1 }
+          if (a < b) { return -1}
+          return 0;
+        })
+
         this.loading = false;
       });
   }
 
-  filterCompanies(searchText: string) {
-    this.filterBox = true;
-    this.filter = searchText;
+  
+  filterCompanies(filterText: string) {
+    this.filterBoxWithResults = true;
+    this.filterBoxText = true;
     this.noFilterTable = false;
-    if(searchText == ''){
-      this.resetFilter();
+    this.filter = filterText;
+    if(filterText == ''){
+      this.resetFilterText();
     }
     this.filteredCompanies.data = this.tableDisplay.data.filter((company) => {
-      return company.name.toLowerCase().includes(searchText.toLowerCase());
-    });
+      return company.name.toLowerCase().includes(filterText.toLowerCase());
+    }); 
+}
+
+filterTableByCountry() {
+  if (this.selectedCountry === 'Tutti gli stati') {
+    this.filteredCompanies.data = this.tableDisplay.data;
+  } else {
+    this.filteredCompanies.data = this.tableDisplay.data.filter(company => company.country === this.selectedCountry);
+  }
+  this.filterBoxWithResults = true;
+  this.noFilterTable = false;
+  this.filterBoxCountry = true;
 }
   
-  resetFilter() {
-    this.filterBox = false;
-    this.searchText = '';
+  resetFilterText() {
+    this.filterBoxText = false;
+    this.filterText = '';
     this.noFilterTable = true;
+    if(this.filterBoxText == false && this.filterBoxCountry == false){
+      this.filterBoxWithResults = false;
+    }
+  }  
+  resetFilterCountry() {
+    this.filterBoxCountry = false;
+    this.selectedCountry = 'Tutti gli stati';
+    this.noFilterTable = true;
+    if(this.filterBoxText == false && this.filterBoxCountry == false){
+      this.filterBoxWithResults = false;
+    }
   }  
  
   openViewOptions(){
@@ -163,7 +199,6 @@ export class CompaniesComponent implements OnInit {
     } else {
       this.viewOptions = false;
     }
-
   }
 
   visibleOrInvisible(i: number){
@@ -180,4 +215,6 @@ export class CompaniesComponent implements OnInit {
   displayAll(){
     this.visibleColumns.fill(true);
   }
+
+  
 }
